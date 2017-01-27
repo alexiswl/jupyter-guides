93532920d3e601dce255b0d9d55d585c78887db6
alexiswl <awlucattini@icloud.com>
2017-01-27 15:48:31 +1100

    updated handler output of parallel processing

--- parallel_process.py
+++ parallel_process.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from subprocess import Popen
+import subprocess
 from itertools import islice
 import random
 
@@ -8,18 +8,20 @@ threads = 5
 
 files = ["output.file%d" % i for i in range(0, threads)]
 
-file_handlers = []
-for i, handler in enumerate(file_handlers):
-    handler = open('output.%d' % i, 'w')
+file_handlers = [None]*threads  
+
+for index, file_output in enumerate(files):
+	file_handlers[index] = open(file_output, 'w')
+
 
 for handler in file_handlers:
     print(handler)
 
 randoms = [random.randrange(1, 10) for i in range(0, 200)]
 
-commands = ["sleep %d && echo %d" % (j, i) for i, j in enumerate(randoms)]
+commands = ["sleep %d && echo %d %d" % (j, i, j) for i, j in enumerate(randoms)]
 
-processes = (Popen(cmd, shell=True) for cmd in commands)
+processes = (subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) for cmd in commands)
 
 running_processes = list(islice(processes, threads))
 
@@ -27,7 +29,8 @@ while running_processes:
     for i, process in enumerate(running_processes):
         if process.poll() is not None:
             out, err = process.communicate()
-            file_handlers[i].write(out + "\n")
+	    print(out)
+            file_handlers[i].write(str(out) + "\n")
             running_processes[i] = next(processes, None)
             if running_processes[i] is None:
                 del running_processes[i]
