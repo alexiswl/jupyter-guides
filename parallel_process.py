6ed04d216222984ca0db4253df1c1eb455bea2ae
alexiswl <alexiswl@student.unimelb.edu.au>
2017-01-27 17:56:44 +1100

    Added parallel processing notebook.

--- parallel_process.py
+++ parallel_process.py
@@ -8,20 +8,18 @@ threads = 5
 
 files = ["output.file%d" % i for i in range(0, threads)]
 
-file_handlers = [None]*threads  
+file_handlers = [None]*threads
 
 for index, file_output in enumerate(files):
-	file_handlers[index] = open(file_output, 'w')
+    file_handlers[index] = open(file_output, 'w')
 
-
-for handler in file_handlers:
-    print(handler)
 
 randoms = [random.randrange(1, 10) for i in range(0, 200)]
 
 commands = ["sleep %d && echo %d %d" % (j, i, j) for i, j in enumerate(randoms)]
 
-processes = (subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) for cmd in commands)
+processes = (subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+             for cmd in commands)
 
 running_processes = list(islice(processes, threads))
 
@@ -29,7 +27,6 @@ while running_processes:
     for i, process in enumerate(running_processes):
         if process.poll() is not None:
             out, err = process.communicate()
-	    print(out)
             file_handlers[i].write(str(out) + "\n")
             running_processes[i] = next(processes, None)
             if running_processes[i] is None:
